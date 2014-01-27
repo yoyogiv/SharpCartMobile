fa9db67cf4c81006dcf0d30b22318769fe78fbd8
yoyogiv <yoram.givon@gmail.com>
2014-01-27 16:22:32 -0600

    settings frgament

---
+++ src/com/sharpcart/android/fragment/SettingsFragment.java
@@ -0,0 +1,16 @@
+package com.sharpcart.android.fragment;
+
+import com.sharpcart.android.R;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsFragment extends PreferenceFragment {
+	   @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+
+	        // Load the preferences from an XML resource
+	        addPreferencesFromResource(R.xml.preferences);
+	    }
+}
